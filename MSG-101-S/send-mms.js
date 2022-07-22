d0128a5f7ea305da095d35ab354ec6ec1da760d7
Loriel-S <loriel099@gmail.com>
2022-07-22 16:08:02 +0200

    Edits SMS env variables

--- MSG-101-S/send-mms.js
+++ MSG-101-S/send-mms.js
@@ -1,5 +1,6 @@
-const accountSid = "ACXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX";
-const authToken = "your_auth_token";
+require("dotenv").config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 client.messages
