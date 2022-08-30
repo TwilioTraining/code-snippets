b54fc80bd93298381f350dc93c02e62a32c9d408
Loriel-S <loriel099@gmail.com>
2022-08-30 17:27:29 +0200

    Adds dollar signs to commands

--- CON-101-S/send-img-curl.sh
+++ CON-101-S/send-img-curl.sh
@@ -1,1 +1,1 @@
-curl -u "<account_sid>:<account_secret>" -X POST https://conversations.twilio.com/v1/Conversations/<conversation_sid>/Messages -d MediaSid=<media_sid>
+$ curl -u "<account_sid>:<account_secret>" -X POST https://conversations.twilio.com/v1/Conversations/<conversation_sid>/Messages -d MediaSid=<media_sid>
